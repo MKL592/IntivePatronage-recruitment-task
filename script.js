9a96a73118274dc476e2bfe9dea4790aed366889
MKL592 <artur-panasiuk592@wp.pl>
2021-02-07 22:01:52 +0100

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+/**
+ * Takes an object/array and returns number of elements.
+ * @author Artur Panasiuk <artur-panasiuk592@wp.pl>
+ * @param {object} oInput Object/array input.
+ * @returns {number}
+ */
 function countAllObjectItems(oInput){
     let amountOfObjectElements = 0;
     for(amount in oInput) {
@@ -5,24 +11,41 @@ function countAllObjectItems(oInput){
     }
     return amountOfObjectElements;
 }
-
+/**
+ * Counts all given keys by given value and returns number.
+ * @param {object} oInput Input Object/array.
+ * @param {string} sKey Checks this key for specific value.
+ * @param {string} sValue Value that you want to search.
+ * @returns {number}
+ */
 function countObjectValues(oInput, sKey, sValue){
     let filterObject = oInput.filter(ob => ob[sKey] === sValue);
     return countAllObjectItems(filterObject);
 }
-
+/**
+ * Changes content of given HTML tag by given value.
+ * @param {string} sTagId HTML tag.
+ * @param {string} sValue Content that you want sTagId element to be.
+ */
 function changeTextById(sTagId, sValue){
     let newTextNode = sValue;
     document.getElementById(sTagId).innerHTML = newTextNode;
 }
-
+/**
+ * Refreshes all values on frontend, that have been changed by other operations.
+ */
 function refreshCounters(){
     changeTextById(
         "moviesCounterAll", countAllObjectItems(moviesData));
     changeTextById(
         "moviesCounterSeen", countObjectValues(moviesData, "seen", "T"));
 }
-
+/**
+ * Creates buttons for showMovieData functions
+ * @param {string} sIsSeen Passed value to check moviesData.seen
+ * @param {number} nLoopIteration Passed iteration number from function
+ * @returns {object} Returns ready to append button object
+ */
 function createButton(sIsSeen, nLoopIteration){
     let newButton = document.createElement("button");
     if(sIsSeen == "T"){
@@ -51,7 +74,9 @@ function createButton(sIsSeen, nLoopIteration){
 
     return newButton;
 }
-
+/**
+ * For every object in moviesData array, creates li tag consisting of button, title, year, genre and summary. Li tags are connected to html class="moviesList".
+ */
 function showMovieData(){
     const brTag = document.createElement("br");
     let amount = 0;
@@ -75,7 +100,9 @@ function showMovieData(){
        document.getElementById("moviesList").appendChild(newText);
     }
 }
-
+/**
+ * Called in HTML by body onload function. Runs only once.
+ */
 function onWebsiteStart(){
     refreshCounters();
     showMovieData();
