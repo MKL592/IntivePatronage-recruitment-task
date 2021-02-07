7e5aec50e8dd3510e1de9caab2e7e8ddae2df21c
MKL592 <artur-panasiuk592@wp.pl>
2021-02-07 21:22:34 +0100

    Shorter and more organised functions

--- script.js
+++ script.js
@@ -23,46 +23,52 @@ function refreshCounters(){
         "moviesCounterSeen", countObjectValues(moviesData, "seen", "T"));
 }
 
+function createButton(sIsSeen, nLoopIteration){
+    let newButton = document.createElement("button");
+    if(sIsSeen == "T"){
+        newButton.innerHTML = "✔";
+    }else if(sIsSeen == "F"){
+        newButton.innerHTML = "✖";
+    }else{
+        newButton.innerHTML = "{ERROR}";
+    }
+    (function(index){
+        newButton.addEventListener("click", function(){
+        if(newButton.innerHTML == "✔"){
+            console.log(index);
+            newButton.innerHTML = "✖";
+            moviesData[index].seen = "F";
+            refreshCounters();
+        }else if(newButton.innerHTML == "✖"){
+            newButton.innerHTML = "✔";
+            moviesData[index].seen = "T";
+            refreshCounters();
+        }else{
+            console.log("ERROR");
+        }
+       })
+    })(nLoopIteration)
+
+    return newButton;
+}
+
 function showMovieData(){
     const brTag = document.createElement("br");
     let amount = 0;
     for(amount in moviesData){
         let newText = document.createElement("li");
-        let newButton = document.createElement("button");
         let seen = moviesData[amount].seen;
+        //let IconButton = createButton(seen, amount);
 
         let dataInfo = {
+            "currentButton": createButton(seen, amount),
             "currentTitle": document.createTextNode(moviesData[amount].title),
             "currentYear": document.createTextNode(moviesData[amount].year),
             "currentGenre": document.createTextNode(moviesData[amount].genre),
             "currentSummary": document.createTextNode(moviesData[amount].summary)
         }
 
-        if(seen == "T"){
-            newButton.innerHTML = "✔";
-        }else if(seen == "F"){
-            newButton.innerHTML = "✖";
-        }else{
-            newButton.innerHTML = "{ERROR}";
-        }
-
-        (function(index){
-           newButton.addEventListener("click", function(){
-            if(newButton.innerHTML == "✔"){
-                newButton.innerHTML = "✖";
-                moviesData[index].seen = "F";
-                refreshCounters();
-            }else if(newButton.innerHTML == "✖"){
-                newButton.innerHTML = "✔";
-                moviesData[index].seen = "T";
-                refreshCounters();
-            }else{
-                console.log("ERROR");
-            }
-           })
-        })(amount)
-
-        newText.appendChild(newButton);
+        //newText.appendChild(IconButton);
 
         for(iteration in dataInfo){
             newText.appendChild(brTag.cloneNode(true));
